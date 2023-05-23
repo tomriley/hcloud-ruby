e9ae2198eee4ade3f6a66e7be958c5175b4c1179
Tom Riley <tom@smallroomsoftware.com>
2023-05-23 19:26:01 +0900

    Update client.rb
    Fixed

--- lib/hcloud/client.rb
+++ lib/hcloud/client.rb
@@ -155,8 +155,8 @@ module Hcloud
       path << "?#{q.join('&')}"
       r = Typhoeus::Request.new(
         "https://api.hetzner.cloud/v1/#{path}",
-        timeout: 5,
         {
+          timeout: 5,
           headers: {
             'Authorization' => "Bearer #{token}",
             'User-Agent' => user_agent,
